3d5c603b8077fe6a8835b9f67765d5493d75191e
jonariv09 <ingjoseagustinnarvaez@gmail.com>
2020-03-27 14:52:55 -0600

    Adding google-passport and facebook-passport configuration

---
+++ config/oauth/google-passport.js
@@ -0,0 +1,53 @@
+require('dotenv').config()
+const GoogleStrategy = require('passport-google-oauth').Strategy()
+const User = require('../../components/user/model')
+
+const {
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET
+} = process.env
+
+
+module.exports = function(passport) {
+  passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback",
+    passReqToCallback: true
+  }, function(req, token, tokenSecret, profile, done) {
+    process.nextTick(() => {
+      if(!req.user) {
+        User.findOne({ 'google.id': profile.id }, (err, user) => {
+          if(err) return done(err)
+          if(user)
+            return done(null, user)
+          else {
+            const newUser = new User({
+                'google.id': profile.id,
+                'google.token': token,
+                'google.email': profile.emails[0].value,
+                'google.name': profile.name
+            })
+
+            newUser.save(err => {
+              if(err) throw err
+              return done(null, newUser)
+            })
+          }
+        })
+      } else {
+        const user = req.user
+        user.google.id = profile.id
+        user.google.token = profile.token
+        user.google.name = profile.name
+        user.google.email = profile.email[0].value
+
+        user.save(function(err) {
+          if(err)
+            throw err
+          return done(null, user)
+        })
+      }
+    })
+  }))
+}
